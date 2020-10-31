daf904fafc8d7bddbd68c632b50ff9bfe791e932
DPadilla <dpadillafonseca@gmail.com>
2020-10-31 11:28:23 -0500

    fixed request http to https

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -2,7 +2,7 @@
 
 export const getGifts = async ( category ) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=DjapK5UHvaMEDK2ZX6qd5pxFN2J3fNqZ`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=DjapK5UHvaMEDK2ZX6qd5pxFN2J3fNqZ`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
